4dfb1abd1dcd0dae4026c3a701aec02be7f461c3
Luis-Bautista-SE <lb9054264@gmail.com>
2026-05-04 14:08:27 -0700

    Update test_student.py

--- test_student.py
+++ test_student.py
@@ -1,1 +1,28 @@
+import unittest
+from proj2 import *
+
+class TestRegionFunctions(unittest.TestCase):
+
+
+    def test_read_csv(self):
+        head = read_csv_lines("sample2.csv")
+        self.assertIsNotNone(head)
+
+    def test_listlen(self):
+        head = read_csv_lines("sample2.csv")
+        self.assertEqual(listlen(head), 8)
+
+    def test_filter_year(self):
+        head = read_csv_lines("sample2.csv")
+        result = filter_rows(head, "year", "greater_than", 2010)
+        # Count results
+        self.assertEqual(listlen(result), 2)
+
+    def test_filter_country(self):
+        head = read_csv_lines("sample2.csv")
+        result = filter_rows(head, "country", "equal", "USA")
+        self.assertEqual(listlen(result), 3)
+
+if __name__ == '__main__':
+    unittest.main()
 
